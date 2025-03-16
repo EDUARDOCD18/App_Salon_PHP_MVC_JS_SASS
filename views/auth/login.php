44ec085e9fbe3072ac6170405d2a7371dbc964eb
eduardocd18 <javierecd@gmail.com>
2025-03-16 17:47:40 -0400

    446. Creando el Formulario de Autenticación

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,29 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form action="/" class="formulario" method="post">
+    <div class="campo">
+        <label for="email">Correo</label>
+        <input
+            type="email"
+            placeholder="Tu correo electrónico"
+            id="email"
+            name="email" />
+    </div>
+
+    <div class="campo">
+        <label for="password">Contraseña</label>
+        <input
+            type="password"
+            placeholder="Tu contraseña"
+            id="passwordd¡"
+            name="password" />
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿No te has registrado? Crea tu cuenta</a>
+    <a href="/olvide">¿Olvidaste te contraseña? Recupérala aquí</a>
+</div>
