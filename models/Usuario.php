3fad7097f69fe1025afd1e7bdc818de7d2b5d6b6
eduardocd18 <javierecd@gmail.com>
2025-03-23 10:25:08 -0400

    458. Creando el Modelo de Usuarios

---
+++ models/Usuario.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Models;
+
+use Model\ActiveRecord;
+
+class Usuario extends ActiveRecord
+{
+
+    // Base de datos
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'nombre', 'apellido', 'email', 'password', 'telefono', 'admin', 'confirmado', 'token'];
+
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $telefono;
+    public $admin;
+    public $confirmado;
+    public $token;
+
+    // Constructor
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->telefono = $args['telefono'] ?? '';
+        $this->admin = $args['admin'] ?? null;
+        $this->confirmado = $args['cofirmado'] ?? null;
+        $this->token = $args['token'] ?? '';
+    }
+}
