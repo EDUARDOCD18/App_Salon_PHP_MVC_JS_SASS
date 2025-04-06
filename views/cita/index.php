c49afb24f7c0923d2fd79e2a7b2e5358d472f214
eduardocd18 <javierecd@gmail.com>
2025-04-06 19:11:22 -0400

    483. Creando Routing y Controllers para Citas

---
+++ views/cita/index.php
@@ -0,0 +1,4 @@
+<!-- VISTA PARA ELEGIR LOS SERVICIOS A AGENDAR EN UNA MISMA CITA -->
+
+<h1 class="nombre-pagina">Registra tu nueva cita</h1>
+<p class="descripcion-pagina">Elije tus servicio a continuación</p>
