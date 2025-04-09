8c3c9c8363798a7c916119b58686126900a635af
eduardocd18 <javierecd@gmail.com>
2025-04-09 03:49:04 -0400

    484. Añadiendo los Contenedores Principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,4 +1,22 @@
 <!-- VISTA PARA ELEGIR LOS SERVICIOS A AGENDAR EN UNA MISMA CITA -->
 
 <h1 class="nombre-pagina">Registra tu nueva cita</h1>
-<p class="descripcion-pagina">Elije tus servicio a continuación</p>
+<p class="descripcion-pagina">Elije tus servicio y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a continuación</p>
+
+        <div class="listados-servicios" id="servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+
+        <h2>Tus datos y citas</h2>
+        <p>Coloca tus datos y fecha de la cita</p>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verificar que la información sea correcta</p>
+    </div>
+</div>
