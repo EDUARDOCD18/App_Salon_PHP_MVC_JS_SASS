242c7d608439f45bb9d804821eea9bf723d57731
eduardocd18 <javierecd@gmail.com>
2025-04-09 04:08:51 -0400

    485. Mostrar el Nombre del usuario autenticado

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -8,8 +8,13 @@ class CitaController
 {
     public static function index(Router $router)
     {
+
+        if (!$_SESSION['nombre']) {
+            session_start(); /* INICIA LA SESIÓN */
+        }
+
         $router->render('cita/index', [
-            
+            'nombre' => $_SESSION['nombre']
         ]);
     }
 }
--- views/cita/index.php
+++ views/cita/index.php
@@ -6,17 +6,33 @@
 <div id="app">
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuación</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
 
         <div class="listados-servicios" id="servicios"></div>
     </div>
     <div id="paso-2" class="seccion">
 
         <h2>Tus datos y citas</h2>
-        <p>Coloca tus datos y fecha de la cita</p>
+        <p class="text-center">Coloca tus datos y fecha de la cita</p>
+
+        <!-- FORMULARIO PARA EL REGISTRO DE LA CITA -->
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" placeholder="Tu nombre" value="<?php echo $nombre; ?>" disabled>
+            </div>
+            <div class="campo">
+                <label for="nombre">Fecha</label>
+                <input type="date" id="fecha">
+            </div>
+            <div class="campo">
+                <label for="nombre">Hora</label>
+                <input type="time" id="hora">
+            </div>
+        </form>
     </div>
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
-        <p>Verificar que la información sea correcta</p>
+        <p class="text-center">Verificar que la información sea correcta</p>
     </div>
 </div>
