e2c50ef4276a35ee3bab33b104c2300d6ea55503
eduardocd18 <javierecd@gmail.com>
2025-04-11 04:58:32 -0400

    489. Primeros pasos con la Navegación por Tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
-alert('Hola');
+let paso = 1;
+
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+/* INICIAR APP  */
+function iniciarApp() {
+  tabs(); // Cambia de sección cuando se presionen los tabs
+}
+
+/* MOSTRAR LA SECCIÓN */
+function mostrarSeccion() {
+  console.log("Mostrar sección");
+}
+
+/* TABS */
+function tabs() {
+  const botones = document.querySelectorAll(".tabs button");
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", function (e) {
+      paso = parseInt(e.target.dataset.paso);
+
+      mostrarSeccion(); // Mostrar la sección
+    });
+  });
+}
