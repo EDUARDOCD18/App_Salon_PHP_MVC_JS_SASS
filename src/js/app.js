03ca7bf5a49fdae4f983c0604fc0baee54d659ef
eduardocd18 <javierecd@gmail.com>
2025-04-12 14:52:12 -0400

    492. Creando el Paginador

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 function iniciarApp() {
   mostrarSeccion(); // Muestra y oculta las secciones
   tabs(); // Cambia de sección cuando se presionen los tabs
+  botonesPaginador(); // Agrega o quita los botones del paginador
 }
 
 /* MOSTRAR LA SECCIÓN */
@@ -44,6 +45,24 @@ function tabs() {
       paso = parseInt(e.target.dataset.paso);
 
       mostrarSeccion(); // Mostrar la sección
+      botonesPaginador(); // Botones del paginador
     });
   });
+}
+
+/* BOTONES DEL PAGINADOR */
+function botonesPaginador() {
+  const paginaAnterior = document.querySelector("#anterior");
+  const paginaSiguiente = document.querySelector("#siguiente");
+
+  if (paso === 1) {
+    paginaAnterior.classList.add("ocultar");
+    paginaSiguiente.classList.remove("ocultar");
+  } else if (paso === 3) {
+    paginaAnterior.classList.remove("ocultar");
+    paginaSiguiente.classList.add("ocultar");
+  } else {
+    paginaAnterior.classList.remove("ocultar");
+    paginaSiguiente.classList.remove("ocultar");
+  }
 }
