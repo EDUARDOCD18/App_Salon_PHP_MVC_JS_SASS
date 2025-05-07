97aaf3bd0a11f44b60d8de985399cdf44899a02b
eduardocd18 <javierecd@gmail.com>
2025-05-07 05:41:57 -0400

    544. Creando el Formulario para Servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -6,3 +6,11 @@
 include_once __DIR__ . '/../templates/barra.php';
 
 ?>
+
+<!-- Formulario para crear un nuevo servicio -->
+<form action="/servicios/crear" method="POST" class="formulario">
+
+    <?php include_once __DIR__ . '/formulario.php' ?>
+
+    <input type="submit" class="boton" value="Registrar Servicio">
+</form>
---
+++ views/servicios/formulario.php
@@ -0,0 +1,9 @@
+<div class="campo">
+    <label for="nombre">Nombre: </label>
+    <input type="text" name="nombre" id="nombre" placeholder="Nombre del servicio">
+</div>
+
+<div class="campo">
+    <label for="nombre">Precio ($): </label>
+    <input type="number" name="precio" id="precio" placeholder="Precio del servicio">
+</div>
